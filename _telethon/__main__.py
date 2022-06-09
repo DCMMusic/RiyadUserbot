62358eb4ecd23381144b0a69d6ab2db10140c4a1
DCMMusic <102096177+DCMMusic@users.noreply.github.com>
2022-06-09 04:22:30 +0400

    Update __main__.py

--- _telethon/__main__.py
+++ _telethon/__main__.py
@@ -25,5 +25,5 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
 
 app.start()
-print("Telethon User Client Started \nPowered By @TheCodents\n(C) 2021 Jayant Kageri")
+print("Telethon User Client Started \nPowered By @KarabakhUserator\n(C) 2021 Thagiyevvvv")
 app.run_until_disconnected()
